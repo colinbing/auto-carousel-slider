510e274cef721c5f792ec29bd4c10c955328e6ed
Colin Bingham <cbingham@internal.tecore.com>
2020-10-01 17:28:48 -0400

    First draft

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("#logoParade").smoothDivScroll({
+		autoScrollingMode: "always",
+		autoScrollingDirection: "endlessLoopRight",
+		autoScrollingStep: 1,
+		autoScrollingInterval: 25
+	});
+});
