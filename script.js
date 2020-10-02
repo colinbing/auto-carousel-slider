8b0ab82f9b2ffe53581e0930d859f0ccef7938b4
Colin Bingham <cbingham@internal.tecore.com>
2020-10-02 14:06:07 -0400

    Cleaned it up

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$("#logoParade").smoothDivScroll({
-		autoScrollingMode: "always",
-		autoScrollingDirection: "endlessLoopRight",
-		autoScrollingStep: 1,
-		autoScrollingInterval: 25
-	});
-});
